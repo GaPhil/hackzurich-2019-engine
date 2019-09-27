fb1e7bbc48b78ec9ccf50cd7ab91c7ab1fb9c373
Simone Stefani <s.stefani95@gmail.com>
2019-09-28 01:29:15 +0200

    Add Java spark

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,34 +14,38 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static spark.Spark.get;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Instantiates a client
-        try {
-            String text = new String(Files.readAllBytes(Paths.get("interview.txt")));
-
-            SkillsService skillsService = new SkillsService();
-            List<Skill> skills = skillsService.loadSkills();
-            
-            TextProcessor textProcessor = new TextProcessor();
-            AnalyzeEntitiesResponse response = textProcessor.processEntities(text);
-
-            Set<String> entities = new HashSet<String>();
-
-            // Print the response
-            for (Entity entity : response.getEntitiesList()) {
-                for (Skill skill : skills) {
-                    if (entity.getName().toLowerCase().contains(skill.getValue().toLowerCase())) {
-                        entities.add(entity.getName().toLowerCase());
-                    }
-                }
-            }
-
-            for (String ent : entities) {
-                System.out.println("This is what we found: " + ent);
-            }
-        } catch (Exception exception) {
-            System.out.print(exception);
-        }
+        get("/hello", (req, res) -> "Hello World");
+//
+//
+//        try {
+//            String text = new String(Files.readAllBytes(Paths.get("interview.txt")));
+//
+//            SkillsService skillsService = new SkillsService();
+//            List<Skill> skills = skillsService.loadSkills();
+//
+//            TextProcessor textProcessor = new TextProcessor();
+//            AnalyzeEntitiesResponse response = textProcessor.processEntities(text);
+//
+//            Set<String> entities = new HashSet<String>();
+//
+//            // Print the response
+//            for (Entity entity : response.getEntitiesList()) {
+//                for (Skill skill : skills) {
+//                    if (entity.getName().toLowerCase().contains(skill.getValue().toLowerCase())) {
+//                        entities.add(entity.getName().toLowerCase());
+//                    }
+//                }
+//            }
+//
+//            for (String ent : entities) {
+//                System.out.println("This is what we found: " + ent);
+//            }
+//        } catch (Exception exception) {
+//            System.out.print(exception);
+//        }
     }
 }
