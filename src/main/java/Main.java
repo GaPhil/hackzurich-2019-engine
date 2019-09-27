3d77b4480ca473d361c4ec5a22f64368eff27688
Simone Stefani <s.stefani95@gmail.com>
2019-09-28 00:06:28 +0200

    Extract entities

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,47 @@
  * Created by GaPhil on 2019-09-27.
  */
 
-import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.*;
 import com.google.cloud.language.v1.Document.Type;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class Main {
     public static void main(String... args) throws Exception {
         // Instantiates a client
         try {
-            LanguageServiceClient language = LanguageServiceClient.create();
-            // The text to analyze
-            String text = "Hello, world!";
-            Document doc = Document.newBuilder()
-                    .setContent(text).setType(Type.PLAIN_TEXT).build();
+            String text = new String(Files.readAllBytes(Paths.get("interview.txt")));
 
-            // Detects the sentiment of the text
-            Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
-            String content = new String(Files.readAllBytes(Paths.get("interview.txt")));
-            System.out.println(content);
-            System.out.printf("Text: %s%n", text);
-            System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+
+            LanguageServiceClient language = LanguageServiceClient.create();
+
+            AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder()
+                    .setDocument(doc)
+                    .setEncodingType(EncodingType.UTF16)
+                    .build();
+
+            AnalyzeEntitiesResponse response = language.analyzeEntities(request);
+
+            // Print the response
+            for (Entity entity : response.getEntitiesList()) {
+                System.out.printf("Entity: %s\n", entity.getName());
+                System.out.printf("Salience: %.3f\n", entity.getSalience());
+                System.out.println("Metadata: ");
+                for (Map.Entry<String, String> entry : entity.getMetadataMap().entrySet()) {
+                    System.out.printf("%s : %s", entry.getKey(), entry.getValue());
+                }
+                for (EntityMention mention : entity.getMentionsList()) {
+                    System.out.printf("Begin offset: %d\n", mention.getText().getBeginOffset());
+                    System.out.printf("Content: %s\n", mention.getText().getContent());
+                    System.out.printf("Type: %s\n\n", mention.getType());
+                }
+            }
         } catch (Exception exception) {
             System.out.print(exception);
         }
