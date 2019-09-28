9e88cf915dd1e28ba39a977b74f80bbdad2ff62b
GaPhil <gaphil.io@gmail.com>
2019-09-28 07:42:04 +0200

    Rename stuff

--- post.sh
+++ post.sh
@@ -8,13 +8,13 @@ case "$1" in
     f2) f=$(<Transcript-Hard-3.txt) ;;
     f3) f=$(<Transcripts-Easy-1.txt) ;;
     f4) f=$(<Application-Transcript-Easy-2.txt) ;;
-    f5) f=$(<Transcript-Medium\ Difficulty-1.txt) ;;
+    f5) f=$(<Transcript-Medium-Difficulty-1.txt) ;;
     f6) f=$(<Transcripts-Easy-2.txt) ;;
     f7) f=$(<Transcript-Hard-1.txt) ;;
-    f8) f=$(<Transcript-Medium\ Difficulty-2.txt) ;;
+    f8) f=$(<Transcript-Medium-Difficulty-2.txt) ;;
     f9) f=$(<Transcripts-Easy-3.txt) ;;
     f10) f=$(<Transcript-Hard-2.txt) ;;
-    f11) f=$(<Transcript-Medium\ Difficulty-3.txt) ;;
+    f11) f=$(<Transcript-Medium-Difficulty-3.txt) ;;
 
     *) f=$(<Application-Transcript-Easy-1.txt) ;;
 
