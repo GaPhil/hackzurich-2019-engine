39a3465e664cd9b2c7e32a1025b931639e631b30
Simone Stefani <s.stefani95@gmail.com>
2019-09-28 23:41:06 +0200

    Extract core logic

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,64 +1,19 @@
-import com.google.cloud.language.v1.*;
-import me.xdrop.fuzzywuzzy.FuzzySearch;
-import nlp.ExtendedSentence;
-import nlp.Party;
-import nlp.TextProcessor;
-import skill.Skill;
-import skill.SkillsService;
+import com.google.cloud.language.v1.Token;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.List;
 
 public class TestRunner {
     public static void main(String[] args) {
 
         try {
-            SkillsService skillsService = new SkillsService();
-            List<Skill> skills = skillsService.loadSkills();
-
             String text = new String(Files.readAllBytes(Paths.get("interview.txt")));
 
-            TextProcessor textProcessor = new TextProcessor();
-            AnalyzeSyntaxResponse res = textProcessor.processSyntax(text);
-
-            List<Sentence> sentences = res.getSentencesList();
-            List<ExtendedSentence> extendedSentences = toExtendedSentences(sentences, true);
-
-            List<Token> conversationContext = new ArrayList<>();
-            List<Token> approvedSkillTokens = new ArrayList<>();
-
-            for (ExtendedSentence sentence : extendedSentences) {
-                // progress indicator
-                System.out.print(".");
-
-                // run NLP on sentence
-                AnnotateTextResponse annotatedSentence = textProcessor.processAll(sentence.getSentence());
-
-                // get all tokens which are whitelisted skills
-                List<Token> skillTokens = filterSkillTokens(annotatedSentence.getTokensList(), skills);
-
-                // if skills introduced by interviewer or in candidate question then add to context
-                if (sentence.isQuestion() || sentence.isInterviewer()) {
-                    conversationContext.addAll(skillTokens);
-                    continue;
-                }
-
-                // check if answer contains negations
-                boolean hasNegations = sentenceHasNegations(annotatedSentence);
-
-                // select either current tokens or from conversation context
-                if (!hasNegations && !skills.isEmpty()) {
-                    approvedSkillTokens.addAll(skillTokens);
-                } else if (!hasNegations && !conversationContext.isEmpty()) {
-                    approvedSkillTokens.addAll(conversationContext);
-                }
-
-                conversationContext.clear();
-            }
+            List<Token> approvedSkillTokens = TextAnalyzer.analyze(text);
 
             System.out.println("\n\nDetected tokens:");
-            for (String tok : dedupeList(approvedSkillTokens)) {
+            for (String tok : TextAnalyzer.dedupeList(approvedSkillTokens)) {
                 System.out.println(tok);
             }
         } catch (Exception e) {
@@ -66,74 +21,4 @@ public class TestRunner {
         }
 
     }
-
-    static private List<Token> filterSkillTokens(List<Token> tokens, List<Skill> skills) {
-        List<Token> skillTokens = new ArrayList<>();
-
-        for (Token token : tokens) {
-            for (Skill skill : skills) {
-                int score = FuzzySearch.ratio(token.getText().getContent().toLowerCase(), skill.getName().toLowerCase());
-                if (token.getText().getContent().toLowerCase().contains(skill.getName().toLowerCase()) || score > 90) {
-                    skillTokens.add(token);
-                }
-            }
-        }
-
-        return skillTokens;
-    }
-
-    static private Set<String> dedupeList(List<Token> tokens) {
-        Set<String> tokensSet = new HashSet<>();
-
-        for (Token token : tokens) {
-            String tokenName = token.getText().getContent();
-            tokensSet.add(tokenName.trim().replaceAll("[^a-zA-Z0-9 ]", ""));
-        }
-
-        return tokensSet;
-    }
-
-    static private boolean sentenceHasNegations(AnnotateTextResponse annotatedSentence) {
-        boolean hasNegations = false;
-        for (Token token : annotatedSentence.getTokensList()) {
-           if (token.getDependencyEdge().getLabel().equals(DependencyEdge.Label.NEG)) {
-               hasNegations = true;
-           }
-        }
-
-        return hasNegations;
-    }
-
-    static private List<ExtendedSentence> toExtendedSentences(List<Sentence> sentences, boolean explicitParty) {
-        List<ExtendedSentence> extendedSentences = new ArrayList<>();
-
-        Party lastParty = Party.UNKNOWN;
-
-        for (Sentence s : sentences) {
-            String text = s.getText().getContent().toLowerCase();
-
-            Party party;
-            if (explicitParty && text.startsWith("candidate:")) {
-                text = text.replaceFirst("candidate: ", "");
-                lastParty = Party.CANDIDATE;
-                party = Party.CANDIDATE;
-            } else if (explicitParty && text.startsWith("interviewer:")) {
-                text = text.replaceFirst("interviewer: ", "");
-                lastParty = Party.INTERVIEWER;
-                party = Party.INTERVIEWER;
-            } else if (explicitParty) {
-                party = lastParty;
-            } else {
-                party = Party.UNKNOWN;
-            }
-            ExtendedSentence ext = new ExtendedSentence(text);
-            ext.setParty(party);
-            ext.setQuestion(text.endsWith("?"));
-            extendedSentences.add(ext);
-        }
-
-        return extendedSentences;
-    }
-
-
 }
