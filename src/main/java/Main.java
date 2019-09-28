f00cdbe11101301e387a2af9f78dd5de3439ccc1
GaPhil <gaphil.io@gmail.com>
2019-09-28 02:29:04 +0200

    Add analyse endpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,48 +8,35 @@ import nlp.TextProcessor;
 import skill.Skill;
 import skill.SkillsService;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
 
+import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class Main {
 
-    private final static Logger LOG = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) throws Exception {
         post("/api/analyze", (request, response) -> new Skill("Hello World"), new JsonTransformer());
-//
-//
-//        try {
-//            String text = new String(Files.readAllBytes(Paths.get("interview.txt")));
-//
-//            SkillsService skillsService = new SkillsService();
-//            List<Skill> skills = skillsService.loadSkills();
-//
-//            TextProcessor textProcessor = new TextProcessor();
-//            AnalyzeEntitiesResponse response = textProcessor.processEntities(text);
-//
-//            Set<String> entities = new HashSet<String>();
-//
-//            // Print the response
-//            for (Entity entity : response.getEntitiesList()) {
-//                for (Skill skill : skills) {
-//                    if (entity.getName().toLowerCase().contains(skill.getValue().toLowerCase())) {
-//                        entities.add(entity.getName().toLowerCase());
-//                    }
-//                }
-//            }
-//
-//            for (String ent : entities) {
-//                System.out.println("This is what we found: " + ent);
-//            }
-//        } catch (Exception exception) {
-//            System.out.print(exception);
-//        }
+
+        SkillsService skillsService = new SkillsService();
+        List<Skill> skills = skillsService.loadSkills();
+
+        get("/skills", (request, response) -> skills, new JsonTransformer());
+
+        post("/analyse", (request, response) -> {
+            TextProcessor textProcessor = new TextProcessor();
+            AnalyzeEntitiesResponse res = textProcessor.processEntities(request.body());
+            Set<String> entities = new HashSet<>();
+            for (Entity entity : res.getEntitiesList()) {
+                for (Skill skill : skills) {
+                    if (entity.getName().toLowerCase().contains(skill.getValue().toLowerCase())) {
+                        entities.add(entity.getName().toLowerCase());
+                    }
+                }
+            }
+            return entities;
+        }, new JsonTransformer());
     }
 }
